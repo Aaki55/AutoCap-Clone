082b8f1eb9d1dac6051de5353fff01048251f48c
Aaki55 <aaki849056@gmail.com>
2024-08-14 08:16:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 
 class Config(object):
       BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
+      API_ID = int(os.environ.get("APP_ID", 17584090))
+      API_HASH = os.environ.get("3f779654fdd3b9278fd95869aabf4593")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
-      ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
-      DB_URL = os.environ.get("DATABASE_URL", "")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "DeoxysX")
+      ADMIN_ID = int(os.environ.get("ADMIN_ID", 7252430326 )) 
+      DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Aaki8490:vnCMn6F5l8nqXxFe@cluster0.jlgdebe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
